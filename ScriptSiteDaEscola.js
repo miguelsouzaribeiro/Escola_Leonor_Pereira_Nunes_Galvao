5715c64dc22e4f3c30266874a4eba14c28819c75
Miguel Ribeiro <miguelsouzaribeiro@gmail.com>
2020-11-11 15:39:28 -0300

    :zap: Começo do modal pelo VueJS :zap:
    Signed-off-by: Miguel Ribeiro <miguelsouzaribeiro@gmail.com>

--- ScriptSiteDaEscola.js
+++ ScriptSiteDaEscola.js
@@ -1,31 +1,75 @@
 
 // Inicialização
-$(document).ready(function() {
+$(document).ready(function () {
 
     var valores = new Vue({
         el: '#valores',
         data: {
-          valores: [
-            { id: "1", titulo: "Ensino de Qualidade", icone: "check_circle", corpo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus..." },
-            { id: "2", titulo: "Entre as melhores", icone: "assessment", corpo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus..." },
-            { id: "3", titulo: "Projetos Extra Curricular", icone: "supervisor_account", corpo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus..." }
+            valores: [
+                { id: "1", titulo: "Ensino de Qualidade", icone: "check_circle", corpo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus..." },
+                { id: "2", titulo: "Entre as melhores", icone: "assessment", corpo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus..." },
+                { id: "3", titulo: "Projetos Extra Curricular", icone: "supervisor_account", corpo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus..." }
 
-        ]
-        }
-      })
-
-    var banners = new Vue({
-        el: '#banners-col',
-        data: {
-            banners: [
-                { id: "1", imagem: "img/Banner01.jpeg", descricao: 'alt="Imagem intitucional"' },
-                { id: "2", imagem: "img/Banner02.jpeg", descricao: 'alt="Imagem intitucional"' },
-                { id: "3", imagem: "img/Banner03.jpeg", descricao: 'alt="Imagem intitucional"' }
-    
             ]
         }
+    });
 
-      })
+
+    var ano1A = {
+        nome: "1°Ano A",
+        materias: [
+            { id: "2", materia: "Português", link: "http://xxxx" },
+            { id: "3", materia: "Matemática", link: "http://xxxx" },
+            { id: "4", materia: "Ciências", link: "http://xxxx" },
+            { id: "5", materia: "História", link: "http://xxxx" },
+            { id: "6", materia: "Geografia", link: "http://xxxx" },
+            { id: "7", materia: "Inglês", link: "http://xxxx" },
+            { id: "8", materia: "Arte", link: "http://xxxx" },
+            { id: "9", materia: "Educação Fisíca", link: "http://xxxx" },
+            { id: "10", materia: "Enriquecimento Curricular", link: "http://xxxx" }
+        ]
+    };
+
+    var ano1B = {
+        nome: "1°Ano B",
+        materias: [
+            { id: "2", materia: "Português", link: "http://xxxx" },
+            { id: "3", materia: "Matemática", link: "http://xxxx" },
+            { id: "4", materia: "Ciências", link: "http://xxxx" },
+            { id: "5", materia: "História", link: "http://xxxx" },
+            { id: "6", materia: "Geografia", link: "http://xxxx" },
+            { id: "7", materia: "Inglês", link: "http://xxxx" },
+            { id: "8", materia: "Arte", link: "http://xxxx" },
+            { id: "9", materia: "Educação Fisíca", link: "http://xxxx" },
+            { id: "10", materia: "Enriquecimento Curricular", link: "http://xxxx" }
+        ]
+    };
+
+    var ano1C = {
+        nome: "1°Ano C",
+        materias: [
+            { id: "2", materia: "Português", link: "http://xxxx" },
+            { id: "3", materia: "Matemática", link: "http://xxxx" },
+            { id: "4", materia: "Ciências", link: "http://xxxx" },
+            { id: "5", materia: "História", link: "http://xxxx" },
+            { id: "6", materia: "Geografia", link: "http://xxxx" },
+            { id: "7", materia: "Inglês", link: "http://xxxx" },
+            { id: "8", materia: "Arte", link: "http://xxxx" },
+            { id: "9", materia: "Educação Fisíca", link: "http://xxxx" },
+            { id: "10", materia: "Enriquecimento Curricular", link: "http://xxxx" }
+        ]
+    };
+
+    var series = new Vue({
+        el: '#seriesXXXX',
+        data: {
+            series: [
+                ano1A,
+                ano1B,
+                ano1C
+            ]
+        }
+    })
 
     // Menu Mobile
     $(".sidenav").sidenav();
@@ -43,11 +87,11 @@ $(document).ready(function() {
     // Tabs
     $('.tabs').tabs();
     // Esconder Menu ao clicar
-    $(".hide-menu").click(function() {
+    $(".hide-menu").click(function () {
         $(".sidenav").sidenav("close");
     });
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(window).scrollTop() > 100) {
             $(".navbar").addClass("nav-color");
         }
